922c370b147e9015f223215264e9bdcffb30755a
Josh Gillham <usajoshgillham@gmail.com>
2013-01-22 19:21:30 -0700

    Wrote it handle threading in several modes.

--- Assignment1/Counter.java
+++ Assignment1/Counter.java
@@ -10,20 +10,22 @@ public class Counter implements java.lang.Runnable {
     /** Holds the current value to add to sum. */
     public static long num;
     /** Holds N, the number of integers to sum. */
-    final public static int size;
+    final public static int size = 10000;
+    public static boolean runSynchronized = false;
     
     /**
      * Perform one computation: (1)add one to num and then (2)add num into sum.
      */
     static void count() {
-        
+        num = 1;
+        sum += num;
     }
     
     /**
      * A synchronized version of count().
      */
-    static void countSynched() {
-        
+    synchronized static void countSynched() {
+        ++num;
     }
     
     /**
@@ -38,27 +40,43 @@ public class Counter implements java.lang.Runnable {
      * 5. Prints results
      */
     static void main( String[] args ) {
-        
+        Counter single = new Counter();
+        for( int i = 0; i < 8; ++i ) {
+            new Thread( new Counter() ).start();
+        }
+        Counter.runSynchronized = true;
+        for( int i = 0; i < 8; ++i ) {
+            new Thread( new Counter() ).start();
+        }
     }
     
     /**
      * Called from run() to repeatedly call count().
      */
     void go( int limit) {
-        
+        for( int i = 0; i < limit; ++i ) {
+            count();
+        }
     }
     
     /**
      * A synchronized version of go().
      */
-    void goSynched( int limit ) {
-        
+    synchronized void goSynched( int limit ) {
+        for( int i = 0; i < limit; ++i ) {
+            count();
+        }
     }
     
     /**
      * Called by the Thread to begin computation.
      */
     public void run() {
-        
+        if ( !runSynchronized ) {
+            go( size );
+        }
+        else {
+            goSynched();
+        }
     }
 }
