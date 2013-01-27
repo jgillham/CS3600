ea736a2b16aae98da473c7289ad24685335ed70b
Josh Gillham <usajoshgillham@gmail.com>
2013-01-27 14:54:24 -0700

    Counter now runs a few tests.

--- Assignment1/Counter.java
+++ Assignment1/Counter.java
@@ -5,27 +5,35 @@
  * @version 1-16-13
  */
 public class Counter implements java.lang.Runnable {
+    
     /** Holds the running total. */
-    public static long sum;
+    public static long sum = 0;
     /** Holds the current value to add to sum. */
-    public static long num;
+    public static long num = 0;
     /** Holds N, the number of integers to sum. */
-    final public static int size = 10000;
+    final public static int size = 10000000;
     public static boolean runSynchronized = false;
     
     /**
      * Perform one computation: (1)add one to num and then (2)add num into sum.
      */
-    static void count() {
+    void count() {
+        //System.out.println( "Count" );
         num = 1;
         sum += num;
+            //System.out.println( "num: " + Counter.num );
+            //System.out.println( "sum: " + Counter.sum );
     }
     
     /**
      * A synchronized version of count().
      */
-    synchronized static void countSynched() {
-        ++num;
+    void countSynched() {
+        //System.out.println( "countSynched" );
+        synchronized ( Counter.class ) {
+            num = 1;
+            sum += num;
+        }
     }
     
     /**
@@ -40,13 +48,61 @@ public class Counter implements java.lang.Runnable {
      * 5. Prints results
      */
     static void main( String[] args ) {
-        Counter single = new Counter();
-        for( int i = 0; i < 8; ++i ) {
-            new Thread( new Counter() ).start();
+        final int THREADS = 2;
+        Counter.num = 0;
+        Counter.sum = 0;
+        Counter.runSynchronized = false;
+        try {
+            long startTime = System.nanoTime();
+            Thread thread = new Thread( new Counter() );
+            thread.start();
+            thread.join();
+            System.out.println( "Seconds: " + ( (System.nanoTime() - startTime) ) );
+            System.out.println( "num: " + Counter.num );
+            System.out.println( "sum: " + Counter.sum );
+            
         }
+        catch ( Exception e ) {
+        }
+        Counter.num = 0;
+        Counter.sum = 0;
+        Counter.runSynchronized = false;
+        try {
+            long startTime = System.nanoTime();
+            Thread[] array = new Thread[THREADS];
+            for( int i = 0; i < THREADS; ++i ) {
+                array[i] = new Thread( new Counter() );
+                array[i].start();
+            }
+            for( int i = 0; i < THREADS; ++i ) {
+                array[i].join();
+            }
+            System.out.println( "Seconds: " + ( (System.nanoTime() - startTime)) );
+            System.out.println( "num: " + Counter.num );
+            System.out.println( "sum: " + Counter.sum );
+            
+        }
+        catch ( Exception e ) {
+        }
+        Counter.num = 0;
+        Counter.sum = 0;
         Counter.runSynchronized = true;
-        for( int i = 0; i < 8; ++i ) {
-            new Thread( new Counter() ).start();
+        try {
+            long startTime = System.nanoTime();
+            Thread[] array = new Thread[THREADS];
+            for( int i = 0; i < THREADS; ++i ) {
+                array[i] = new Thread( new Counter() );
+                array[i].start();
+            }
+            for( int i = 0; i < THREADS; ++i ) {
+                array[i].join();
+            }
+            System.out.println( "Seconds: " + ( (System.nanoTime() - startTime)) );
+            System.out.println( "num: " + Counter.num );
+            System.out.println( "sum: " + Counter.sum );
+            
+        }
+        catch ( Exception e ) {
         }
     }
     
@@ -54,6 +110,7 @@ public class Counter implements java.lang.Runnable {
      * Called from run() to repeatedly call count().
      */
     void go( int limit) {
+        //System.out.println( "Count" );
         for( int i = 0; i < limit; ++i ) {
             count();
         }
@@ -63,8 +120,9 @@ public class Counter implements java.lang.Runnable {
      * A synchronized version of go().
      */
     synchronized void goSynched( int limit ) {
+        System.out.println( "goSynched" );
         for( int i = 0; i < limit; ++i ) {
-            count();
+            countSynched();
         }
     }
     
@@ -72,11 +130,14 @@ public class Counter implements java.lang.Runnable {
      * Called by the Thread to begin computation.
      */
     public void run() {
+        //System.out.println( "Run" );
         if ( !runSynchronized ) {
+            //System.out.println( "Not Synched" );
             go( size );
         }
         else {
-            goSynched();
+            goSynched( size );
         }
+        
     }
 }
