a4c9b12885391052b3b3ed8548b0b2b26a0737e6
Josh Gillham <usajoshgillham@gmail.com>
2013-02-06 12:10:28 -0700

    Refactered and cleaned up.

--- Assignment2/BrokerImplementation.java
+++ Assignment2/BrokerImplementation.java
@@ -23,8 +23,6 @@ public class BrokerImplementation implements Broker {
         for ( int i = 0; i < IBM.METALS; ++i ) {
             inventory[ i ] = 0;
         }
-
-        //throw new UnsupportedOperationException();
     }
 
     /**
@@ -35,17 +33,12 @@ public class BrokerImplementation implements Broker {
      *  gold, platinum, and uranium desired.
      */
     public void get( int[] order) {
-        Project2.debug( IBM.metalName[ this.specialty ] + 
-            " - get() called" );
-        for ( int l = 0; l < IBM.METALS; ++l ) {
-            Project2.debug( "At " + IBM.metalName[ this.specialty ] + 
-                " has " + IBM.metalName[ l ] + ":" + (this.inventory[ l ] + 
-                this.trading[ l ]) );
-        }
-        // Look for cases where inventory is too low
-        //  and build up enough inventory.
+        // Go through each metal in the order and move the needed inventory
+        //  into the private cache (called trading). The cache protects our
+        //  order from being used.
         int required = 0;
         int i = 0;
+        boolean threadInterrupted = false;
         try {
             for ( ; i < order.length; ++i ) {
                 required += order[ i ];
@@ -57,8 +50,8 @@ public class BrokerImplementation implements Broker {
                             this.wait();
                         }
                     }
-                    // Get the resources from inventory and, if inventory
-                    //  is running low swap with the specialist.
+                    // Get the resources from inventory and, if the inventory
+                    //  is running, low swap with the specialist.
                     else {
                         this.getNonspecialtyResource( i, order[ i ] );
                     }
@@ -67,64 +60,40 @@ public class BrokerImplementation implements Broker {
                     trading[ i ] += order[ i ];
                 }
             }
-            // Send inventory.
-            synchronized ( this ) {
-                for ( --i ; i >= 0; --i ) {
-                    trading[ i ] -= order[ i ];
-                }
-            }
         }
+        // This will occur when getNonspecialtyResource() is interrupted while
+        //  waiting to fill an order. This is usually because Project2 is wanting
+        //  to end the program. In this case, no inventory has been altered for the
+        //  current iteration (i). Therefore, we restore inventory for previous 
+        //  iterations.
         catch ( StopThread e ) {
-            Project2.debug( IBM.metalName[ this.specialty ] + 
-                " - get() STOP THREAD" );
-            // Reclaim inventory.
-            synchronized ( this ) {
-                for ( --i ; i >= 0; --i ) {
-                    trading[ i ] -= order[ i ];
-                    this.loadMetal( i, order[ i ] );
-                }
-            }
-            //e.printStackTrace();
+            threadInterrupted = true;
             throw new StopThread();
         }
         // The exception will occur in the wait() or getNonspecialtyResource()
-        //  functions. In these cases, no inventory has been altered for the
-        //  current iteration (i). Therefore, we restore inventory for
-        //  previous iterations.
+        //  functions. This is usually because Project2 is wanting to end the 
+        //  program. 
         catch ( InterruptedException e ) {
-            Project2.debug( IBM.metalName[ this.specialty ] + 
-                " - get() INTERRUPTED" );
-            // Reclaim inventory.
+            threadInterrupted = true;
+            throw new StopThread();
+        }
+        // In the case of a thread interruption, the program needs to transfer 
+        //  inventory from the cache (called trading) back to the main store. In 
+        //  the case of not being interruped, the program needs only to deduct 
+        //  the inventory from the cache.
+        //
+        // No inventory has been altered for the current iteration (i). Therefore,
+        //  the program only looks at previous iterations.
+        finally {
             synchronized ( this ) {
                 for ( --i ; i >= 0; --i ) {
                     trading[ i ] -= order[ i ];
-                    this.loadMetal( i, order[ i ] );
+                    if ( threadInterrupted ) {
+                        this.loadMetal( i, order[ i ] );
+                    }
                 }
             }
-            //e.printStackTrace();
-            throw new StopThread();
         }
-        catch ( Throwable e ) {
-            Project2.debug( IBM.metalName[ this.specialty ] + 
-                " - get() THROWABLE" );
-            // Reclaim inventory.
-            synchronized ( this ) {
-                for ( --i ; i >= 0; --i ) {
-                    trading[ i ] -= order[ i ];
-                    this.loadMetal( i, order[ i ] );
-                }
-            }
-            //e.printStackTrace();
-            throw new StopThread();
-        }
-        for ( int l = 0; l < IBM.METALS; ++l ) {
-            Project2.debug( "At " + IBM.metalName[ this.specialty ] + 
-                " has " + IBM.metalName[ l ] + ":" + (this.inventory[ l ] + 
-                this.trading[ l ]) );
-        }
-        Project2.debug( IBM.metalName[ this.specialty ] + 
-            " - get() done" );
-        //throw new UnsupportedOperationException();
     }
 
     /**
@@ -134,16 +103,8 @@ public class BrokerImplementation implements Broker {
      * @param ounces is a number of ounces.
      */
     synchronized public void deliver( int ounces ) {
-        Project2.debug( IBM.metalName[ this.specialty ] + 
-            " - deliver() called" );
-        Project2.debug( IBM.metalName[ this.specialty ] + 
-            " receiving " + ounces );
         this.loadMetal( this.specialty, ounces );
-        Project2.debug( IBM.metalName[ this.specialty ] + 
-            " total " + this.inventory[ this.specialty ] );
         this.notifyAll();
-        Project2.debug( IBM.metalName[ this.specialty ] + 
-            " - deliver() done" );
     }
 
     /**
@@ -158,44 +119,24 @@ public class BrokerImplementation implements Broker {
      * @param ounces how many ounces to swap.
      */
     public void swap( int what, int ounces ) {
-        Project2.debug( IBM.metalName[ this.specialty ] + 
-            " - swap() called" );
-        for ( int l = 0; l < IBM.METALS; ++l ) {
-            Project2.debug( "At " + IBM.metalName[ this.specialty ] + 
-                " has " + IBM.metalName[ l ] + ":" + (this.inventory[ l ] + 
-                this.trading[ l ]) );
-        }
-        assert ( what == this.specialty );
         try {
-            Project2.debug( IBM.metalName[ this.specialty ] + 
-                " - swap() called1" );
             // Continue to hold while waiting for more inventory
             //  from the refiner.
             synchronized ( this ) {
                 while ( !this.takeMetal( this.specialty, ounces ) ) {
                     this.wait();
-                    Project2.debug(IBM.metalName[ this.specialty ] + 
-                        "swap done waiting");
                 }
+                // Receive swap.
+                this.loadMetal( what, ounces );
             }
-            Project2.debug( IBM.metalName[ this.specialty ] + 
-                " - swap() called2" );
-            // Accept offering.
-            this.loadMetal( what, ounces );
+            
         }
-        // The exception will occur in the wait() function.
-        //  Therefore, no alteration to the inventory occured.
+        // The exception will occur in the wait() function. Therefore,
+        //  the inventory was unchanged. This usually occurs when the 
+        //  main wants to end the program. 
         catch ( InterruptedException e ) {
-            Project2.debug( IBM.metalName[ this.specialty ] + 
-                " - swap() STOP" );
             throw new StopThread();
         }
-        for ( int l = 0; l < IBM.METALS; ++l ) {
-            Project2.debug( "At " + IBM.metalName[ this.specialty ] + 
-                " has " + IBM.metalName[ l ] + ":" + (this.inventory[ l ] + 
-                this.trading[ l ]) );
-        }
-        Project2.debug( IBM.metalName[ this.specialty ] + " - swap() done" );
     }
 
     /**
@@ -208,9 +149,6 @@ public class BrokerImplementation implements Broker {
      */
     synchronized public void getAmountOnHand( int[] result ) {
         for ( int i = 0; i < IBM.METALS; ++i ) {
-            Project2.debug( "At " + IBM.metalName[ this.specialty ] + 
-                " has " + IBM.metalName[ i ] + ":" + (this.inventory[ i ] + 
-                this.trading[ i ]) );
             result[ i ] = this.inventory[ i ] + this.trading[ i ];
         }
     }
@@ -252,25 +190,12 @@ public class BrokerImplementation implements Broker {
      * @param totalAmount is the ounces needed.
      */
     public void getNonspecialtyResource( int what, int totalAmount ) {
-        Project2.debug( IBM.metalName[ this.specialty ] + 
-            " - getNonspecialtyResource() called" );
-        for ( int l = 0; l < IBM.METALS; ++l ) {
-            Project2.debug( "At " + IBM.metalName[ this.specialty ] + 
-                " has " + 
-                IBM.metalName[ l ] + ":" + (this.inventory[ l ] + 
-                this.trading[ l ]) );
-        }
-        assert ( what != this.specialty );
         // Take the inventory and get the amount needed if any.
         int amountNeeded = totalAmount - this.inventory[ what ];
-        Project2.debug( IBM.metalName[ this.specialty ] + 
-            " - getNonspecialtyResource() amountNeeded: " + amountNeeded );
         // Peform a swap if necessary.
         if ( amountNeeded > 0 ) {
-            Project2.debug( IBM.metalName[ this.specialty ] + 
-                " - getNonspecialtyResource() amountNeeded:! " );
             try {
-                // Take the amount we need for the swap.
+                // Take the metals we need to offer the other broker.
                 synchronized ( this ) {
                     while ( !this.takeMetal( this.specialty, amountNeeded ) ) {
                         this.wait();
@@ -279,60 +204,33 @@ public class BrokerImplementation implements Broker {
                 }
             }
             // The exception will occur in the wait() functions.
-            //  In that case, no inventory has been changed.
+            //  In that case, the inventory was unchanged.
             catch ( InterruptedException e ) {
                 throw new StopThread();
             }
-            for ( int l = 0; l < IBM.METALS; ++l ) {
-                Project2.debug( "At " + IBM.metalName[ this.specialty ] + 
-                    " has " + 
-                    IBM.metalName[ l ] + ":" + (this.inventory[ l ] + 
-                    this.trading[ l ]) );
-            }
-            Project2.debug( IBM.metalName[ this.specialty ] + 
-                " - getNonspecialtyResource() amountNeeded:& " );
             try {
                 // Get the metals from the specialist.
                 Project2.specialist( what ).swap(
                     this.specialty, amountNeeded
                 );
-                for ( int l = 0; l < IBM.METALS; ++l ) {
-                    Project2.debug( "At " + IBM.metalName[ this.specialty ] + 
-                        " has " + 
-                        IBM.metalName[ l ] + ":" + (this.inventory[ l ] + 
-                        this.trading[ l ]) );
-                }
                 // Swap is complete. Load received metals.
-                synchronized ( this ) {
-                    this.loadMetal( what, amountNeeded );
-                }
+                this.loadMetal( what, amountNeeded );
 
             }
             // The exception will occur in the swap() function.
-            //  In that case, restore metals that were taken.
+            //  In that case, restore metals that were offered,
+            //  but, not used.
             catch ( StopThread e ) {
-                Project2.debug( IBM.metalName[ this.specialty ] + 
-                    " - getNonspecialtyResource() STOPTHREAD" );
-                // Restore specialty offered, but, not used.
                 this.loadMetal( this.specialty, amountNeeded );
                 throw new StopThread();
             }
+            // Always remove the offered metals from the cache.
             finally {
-                Project2.debug( "finally" );
                 synchronized ( this ) {
                     this.trading[ this.specialty ] -= amountNeeded;
-                }
-                for ( int l = 0; l < IBM.METALS; ++l ) {
-                    Project2.debug( "At " + IBM.metalName[ this.specialty ] + 
-                        " has " + 
-                        IBM.metalName[ l ] + ":" + (this.inventory[ l ] + 
-                        this.trading[ l ]) );
                 }
             }
 
         }
-
-        Project2.debug( IBM.metalName[ this.specialty ] +
-            " - getNonspecialtyResource() done" );
     }
 }
