01402879e404f5dd401c4529e1412dba98dea93a
Josh Gillham <usajoshgillham@gmail.com>
2013-03-03 13:48:38 -0700

    Tuned MyRandom.

--- Assignment3/src/MyRandom.java
+++ Assignment3/src/MyRandom.java
@@ -2,14 +2,15 @@ import java.util.Random;
 
 class MyRandom extends Random {
     static private int orderNum = 0;
-    static private int K = 30;
+    static private int K = 300;
     public void newOrder() {
         ++this.orderNum;
-        if ( this.K > 1 ) {
+        if ( this.K > 8 ) {
             this.K /= 2;
         }
     }
     public int nextInt( int id, int metal, int max ) {
-       return this.K * super.nextInt( max ); 
+       return this.K;
+       //* super.nextInt( max ); 
     }
 }
