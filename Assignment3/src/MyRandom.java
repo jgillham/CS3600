0a6c4773c0b9bb24fb24cc82aa7b905fa0e89955
Josh Gillham <usajoshgillham@gmail.com>
2013-03-03 16:41:55 -0700

    Fixed

--- Assignment3/src/MyRandom.java
+++ Assignment3/src/MyRandom.java
@@ -3,13 +3,9 @@ import java.util.Random;
 class MyRandom extends Random {
     static public int MAX = 100;
     static public int MIN = 4;
-    static public int AMOUNT = 50; 
     static public int K = -10;
 
-    private int amount ;
-    public void MyRandom() {
-        this.amount = AMOUNT;
-    }
+    private int amount = 50;
     public void newOrder() {
         int newAmt = this.amount + this.K; 
         if ( newAmt >= MIN && newAmt <= MAX ) {
