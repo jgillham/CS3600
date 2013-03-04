4ddb3a3a3ebc8a67623cb5f5d689030cab52d746
Josh Gillham <usajoshgillham@gmail.com>
2013-03-04 12:38:57 -0700

    Using tighter constraints on the data.

--- Assignment3/src/MyRandom.java
+++ Assignment3/src/MyRandom.java
@@ -1,11 +1,12 @@
 import java.util.Random;
 
 class MyRandom extends Random {
-    static public int MAX = 100;
+    static public int MAX = 200;
     static public int MIN = 4;
     static public int K = -5;
+    static public int VAR = 10;
 
-    private int amount = 50;
+    private int amount = 100;
     public void newOrder() {
         int newAmt = this.amount + this.K; 
         if ( newAmt >= MIN && newAmt <= MAX ) {
@@ -13,6 +14,6 @@ class MyRandom extends Random {
         }
     }
     public int nextInt( int id, int metal, int max ) {
-       return (int)((double)this.amount * (double)super.nextInt( max )/max); 
+       return this.amount + 10 * super.nextInt( max ) / max; 
     }
 }
