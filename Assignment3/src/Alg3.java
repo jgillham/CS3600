a590eb11f4d29a6b740294ed4475dee43dc5c84e
Josh Gillham <usajoshgillham@gmail.com>
2013-03-10 07:55:22 -0600

    Finished part 3 and 4.

--- Assignment3/src/Alg3.java
+++ Assignment3/src/Alg3.java
@@ -2,7 +2,6 @@
 public class Alg3 extends AlgorithmRunner {
     public static void main(String[] args) {
         try{
-            expCount = 1;
             DescendingOrders( 3 );
             AscendingOrders( 3 );
             RandomOrders( 3 );
--- Assignment3/src/Alg4.java
+++ Assignment3/src/Alg4.java
@@ -2,7 +2,6 @@
 public class Alg4 extends AlgorithmRunner {
     public static void main(String[] args) {
         try{
-            expCount = 1;
             DescendingOrders( 4 );
             AscendingOrders( 4 );
             RandomOrders( 4 );
